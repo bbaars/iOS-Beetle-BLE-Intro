c04aa841969f1744315b3a79fb3f28c50b399cfc
Brandon Baars <brandonbaars@gmail.com>
2017-08-15 21:26:01 -0400

    Initial Commit

---
+++ BluetoothTests/BluetoothTests.swift
@@ -0,0 +1,36 @@
+//
+//  BluetoothTests.swift
+//  BluetoothTests
+//
+//  Created by Brandon Baars on 8/15/17.
+//  Copyright © 2017 Brandon Baars. All rights reserved.
+//
+
+import XCTest
+@testable import Bluetooth
+
+class BluetoothTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
